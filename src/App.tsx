3d20a4b147cd56fa362afb41192849619ccb796c
jitesh <jiteshboppana@gmail.com>
2025-05-02 09:39:30 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -174,8 +174,8 @@ export default function App() {
       className="min-h-screen w-full bg-cover bg-center flex items-center justify-center"
       style={{ backgroundImage: 'url("/college-bg.jpg")' }}
     >
-      {/* <div className="backdrop-blur-sm bg-white/90 sm:max-w-xl w-full p-8 rounded-3xl shadow-2xl flex flex-col items-center lift-card border border-zinc-200 relative">
-        <img
+      <div className="backdrop-blur-sm bg-white/90 sm:max-w-xl w-full p-8 rounded-3xl shadow-2xl flex flex-col items-center lift-card border border-zinc-200 relative">
+        {/*<img
           src="/college-logo.png"
           alt="RVR & JC College Logo"
           className="w-24 h-24 mb-2 drop-shadow-lg rounded-full border-4 border-white -mt-24 bg-white"
@@ -247,7 +247,7 @@ export default function App() {
                   value={subj.code}
                   onChange={e => {
                     const next = [...subjects];
-                    next[idx]={ ...next[idx], code: e.target.value; };
+                    next[idx] = { ...next[idx], code: e.target.value; };
                     setSubjects(next);
                   }}
                   placeholder="e.g. MA101"
