7357fe01714a7d154c284f346ce946367abdcf94
jitesh <jiteshboppana@gmail.com>
2025-05-02 09:17:55 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -32,9 +32,6 @@ module.exports = {
           DEFAULT: 'hsl(var(--muted))',
           foreground: 'hsl(var(--muted-foreground))'
         },
-        backgroundImage:{
-            'bg':"url('college-bg.jpg')",
-        }
         accent: {
           DEFAULT: 'hsl(var(--accent))',
           foreground: 'hsl(var(--accent-foreground))'
